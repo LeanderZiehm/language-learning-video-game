4084facb4bb7f8c69e08e4a93d48d93ad69e9347
Leander <leander-arun.ziehm@stud.th-deg.de>
2025-07-01 23:19:38 +0200

    +

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: false
+    open: false,
+     allowedHosts: 'all'
   },
   // Properly handle publicPath for assets
   base: './',
